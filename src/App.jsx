8770a55da86788ff6b97c3d4e1f9bcc26f310793
hubertnshuti <nshutihubert4@gmail.com>
2026-04-11 15:10:54 +0200

    Implement  Nested Routes with dashboard stats and settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 
 function App() {
-  return (
-    <h1 className="text-3xl text-red-500">Hello, World!</h1>
-  )
+  <BrowserRouter>
+    <Routes>
+      <Route />
+      <Route path='/' element={<Dashboard/>}>
+        <Route path='/stats' element={<Stats/>}/>
+        <Route path='/settings' element={<Settings/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 }
 export default App;
+
+
+function Dashboard() {
+  const user = {
+    name: 'John',
+    email: 'john@gmail.com'
+  }
+
+  return (
+    <>
+      <nav>
+        <NavLink to='/stats'>Go to Stats</NavLink>
+        <NavLink to='/settings'>Go to Settings</NavLink>
+      </nav>
+
+      <h1>Hello, this is the main Dashboard</h1>
+      <Outlet context={user}/>
+    </>
+  )
+}
+
+function Stats() {
+  const person = useOutletContext();
+  return <h1>This is stats section the user to test is {person.name} </h1>
+}
+
+function Settings() {
+  return <h1>This is Settings section</h1>
+}
